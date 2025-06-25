f51ea4b421d26b8e2e6cf1a636210d2cecf7309c
nayeong KIM <rlaskdud5625@naver.com>
2025-06-25 13:46:55 +0900

    국회의원 선거.cpp update

--- coding_test/국회의원선거.cpp
+++ coding_test/국회의원선거.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main() {
-	int n, a, b, cnt = 0, max;
+	int n, a, cnt = 0;
 	cin >> n;
-	int *arr = new int[n];
+	vector<int> p;
 	for (int i = 0; i < n; i++) {
 		cin >> a;
-		arr[i] = a;
+		p.push_back(a);
 	}
-	for (int i = 1; i < n; i++) {
-		max = 0;
-		for (int j = 1; j < n; j++) {
-			if (max < arr[j]) {
-				max = arr[j];
-				arr[j] = 0;
-			}
-		}
-		if (arr[0] <= max) {
-			b = (max - arr[0]) / 2 + 1;
-			arr[0] += b;
-			cnt += b;
+
+	p[0] = p[0] - 1;
+	while (1) {
+		int max_idx = max_element(p.begin(), p.end()) - p.begin();
+		if (max_idx == 0) break;
+		else if (max_idx != 0) {
+			p[max_idx] -= 1;
+			p[0] += 1;
+			cnt++;
 		}
 	}
 	cout << cnt;
